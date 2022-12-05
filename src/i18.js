ff71ae60d86b5dc6446aa9eb6d880d02f1d4f413
Anil-Can <anikcan6840@gmail.com>
2022-12-05 19:55:44 +0300

    scroll css eklendi

--- src/i18.js
+++ src/i18.js
@@ -4,7 +4,7 @@ import { initReactI18next } from 'react-i18next';
 i18n
     .use(initReactI18next)
     .init({
-        fallbackLng: 'tr',
+        fallbackLng: 'en',
         resources:{
             en:{
                 translation:
